0a190cabb5467186a1f2368048f4776b9bb6d12e
evenilson <evenilsonlp@gmail.com>
2020-12-01 19:06:57 -0300

    criando o primeiro endpoint GET com express js

--- aulas/backend/node_project_01/config/express.js
+++ aulas/backend/node_project_01/config/express.js
@@ -7,5 +7,14 @@ module.exports = function(){
     //Defindo varial de aplicação
     app.set("port", 8393);
     app.use(express.static("./public"));
+
+    //GET /alunos
+    app.get("/alunos", function(req, res){
+        res.json([
+            {nome:"Victor", sobrenome:"Farias"},
+            {nome:"Luis", sobrenome:"Andrade"},
+        ]);
+    });
+
     return app;             
 }
