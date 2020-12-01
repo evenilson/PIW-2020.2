c15d42ebf7221dddf8a5f0982b0dd09bf31648a7
evenilson <evenilsonlp@gmail.com>
2020-12-01 20:20:34 -0300

    endpoint com parametro + setando status code

--- aulas/backend/node_project_01/app/controllers/alunos.js
+++ aulas/backend/node_project_01/app/controllers/alunos.js
@@ -1,6 +1,20 @@
+let alunos = [
+    {id: 5, nome:"Victor", sobrenome:"Farias"},
+    {id: 10, nome:"Luis", sobrenome:"Andrade"},
+];
+
 module.exports.listarAlunos = function(req, res){
-    res.json([
-        {nome:"Victor", sobrenome:"Farias"},
-        {nome:"Luis", sobrenome:"Andrade"},
-    ]);
+    res.json(alunos);
+}
+
+module.exports.buscarAlunoPorId = function(req, res){
+    let id = req.params.id;
+
+    let aluno = alunos.find(function(aluno){return aluno.id==id;});
+    
+    if(aluno){
+        res.json(aluno);
+    }else{
+        res.status(404).json({mensagem:"Aluno não encontrado"});
+    }
 }
--- aulas/backend/node_project_01/app/routes/alunos.js
+++ aulas/backend/node_project_01/app/routes/alunos.js
@@ -2,4 +2,5 @@ const controller = require("../controllers/alunos")
 
 module.exports = function(app){
     app.get("/alunos", controller.listarAlunos);
+    app.get("/alunos/:id", controller.buscarAlunoPorId);
 }
