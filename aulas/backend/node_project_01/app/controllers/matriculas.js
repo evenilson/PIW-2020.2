c6512ce7d0feb9c0570cbb5f327ff053f421d04f
evenilson <evenilsonlp@gmail.com>
2021-01-14 00:32:08 -0300

    aula de validação de tokens de matriculas

--- aulas/backend/node_project_01/app/controllers/matriculas.js
+++ aulas/backend/node_project_01/app/controllers/matriculas.js
@@ -1,9 +1,15 @@
+const jwt = require("jsonwebtoken")
+
 const Matricula = require("../models/matricula")
 const viewMatricula = require("../views/matriculas")
 
-module.exports.inserirMatricula = function(req, res){
-    let matricula = req.body;
-    let promise = Matricula.create(matricula);
+module.exports.inserirMatricula = function(req, res){ 
+    let id_disciplina = req.body.disciplina;
+    let payload = jwt.decode(req.headers.token);
+    let id_aluno_logado = payload.id;
+
+   
+    let promise = Matricula.create({disciplina: id_disciplina, aluno:id_aluno_logado });
     promise.then(function(matricula){
         res.status(201).json(viewMatricula.render(matricula));
     }).catch(function(error){
@@ -12,7 +18,10 @@ module.exports.inserirMatricula = function(req, res){
 }
 
 module.exports.listarMatriculas = function(req, res){
-    let promise = Matricula.find().populate("disciplina").populate("aluno").exec();
+    let payload = jwt.decode(req.headers.token);
+    let id_aluno_logado = payload.id;
+
+    let promise = Matricula.find({aluno: id_aluno_logado}).populate("disciplina").populate("aluno").exec();
     promise.then(function(matriculas){
         res.status(200).json(viewMatricula.renderMany(matriculas));
     }).catch(function(error){
--- aulas/backend/node_project_01/app/routes/matriculas.js
+++ aulas/backend/node_project_01/app/routes/matriculas.js
@@ -1,6 +1,8 @@
 const controller = require("../controllers/matriculas");
+const controllerAuth = require("../controllers/auth");
 
 module.exports = function(app){
+    app.use("/matriculas", controllerAuth.checar);
     app.post("/matriculas", controller.inserirMatricula);
     app.get("/matriculas", controller.listarMatriculas);
 }
