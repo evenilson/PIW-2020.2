a50b70e0a20087691b5576c1d2010657073ea737
evenilson <evenilsonlp@gmail.com>
2021-01-13 23:25:48 -0300

    aula de criação de token de autenticação com JWT

--- aulas/backend/node_project_01/app/controllers/auth.js
+++ aulas/backend/node_project_01/app/controllers/auth.js
@@ -1,11 +1,14 @@
-const Aluno = require("../models/aluno");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+
+const Aluno = require("../models/aluno");
 
 module.exports.logar = function(req, res){
     Aluno.findOne({matricula: req.body.matricula})
         .then(function(aluno){
             if(bcrypt.compareSync(req.body.senha, aluno.senha)){
-                res.status(200).send("credenciais ok");
+                let token = jwt.sign({id:aluno._id}, "senha_secreta");
+                res.status(200).json({token:token});
             }else{
                 res.status(401).send("credenciais erradas");
             }
