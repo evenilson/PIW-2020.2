7c95e41b22484d06e72b0012c036cb9ddb3d7825
evenilson <evenilsonlp@gmail.com>
2021-01-15 01:51:09 -0300

    validando tokens nos endpoints de usuarios

--- atividades/atividade_03/app/controllers/authController.js
+++ atividades/atividade_03/app/controllers/authController.js
@@ -7,7 +7,7 @@ module.exports.logar = function(req, res){
     usuarioModel.findOne({email: req.body.email})
         .then(function(usuario){
             if(bcrypt.compareSync(req.body.senha, usuario.senha)){
-                let token = jwt.sign({id:usuario._id}, "secret")
+                let token = jwt.sign({id:usuario._id}, "senha_secreta")
                 res.status(200).json({token:token});
             }
             else{
@@ -17,4 +17,17 @@ module.exports.logar = function(req, res){
         .catch(function(error){
             res.status(401).send("credenciais erradas");
         })
+}
+
+module.exports.checar = function(req, res, next){
+    let token = req.headers.token;
+
+    jwt.verify(token, "senha_secreta", function(err, decoded){
+        if(err){
+            res.status(401).send("Token invalido");
+        }else{
+            next();
+        }
+        
+    })
 }
--- atividades/atividade_03/app/routes/usuariosRouter.js
+++ atividades/atividade_03/app/routes/usuariosRouter.js
@@ -5,9 +5,9 @@ module.exports = function(app){
     app.post("/usuarios", usuariosController.inserirUsuario);
     app.post("/usuarios/signin", authController.logar);
 
+    app.use("/usuarios", authController.checar);
     app.get("/usuarios", usuariosController.listarUsuarios);
     app.get("/usuarios/:id", usuariosController.BuscarUsuarioPorId);
     app.get("/usuarios/:id/posts", usuariosController.obterPosts);
-
     app.delete("/usuarios/:id", usuariosController.removerUsuario);
 }
