625bccd4f347b79233cf015b3a4854ed12150abf
evenilson <evenilsonlp@gmail.com>
2021-01-17 02:41:38 -0300

    concertando a remoção de usuários

--- atividades/atividade_03/app/controllers/usuariosController.js
+++ atividades/atividade_03/app/controllers/usuariosController.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 const usuarioModel = require("../models/usuarioModel");
 const usuarioView = require("../views/usuarioView");
@@ -49,14 +50,21 @@ module.exports.inserirUsuario = function(req, res){
 
 module.exports.removerUsuario = function(req, res){
     let id = req.params.id;
-    
-    let promise = usuarioModel.findByIdAndRemove(id);
 
-    promise.then(function(usuario){
-        res.status(200).json(usuarioView.render(usuario));
-    }).catch(function(error){
-        res.status(500).json({mensagem:"Deu ruim!", error:error})
-    })
+    let payload = jwt.decode(req.headers.token);
+    let id_usuario_logado = payload.id;
+    
+    if(id != id_usuario_logado){
+        res.status(401).send("sem permissão");
+    }else{
+        let promise = usuarioModel.findByIdAndDelete(id_usuario_logado);
+
+        promise.then(function(usuario){
+            res.status(200).json(usuarioView.render(usuario));
+        }).catch(function(error){
+            res.status(500).json({mensagem:"Deu ruim!", error:error})
+        })
+    }
 }
 
 module.exports.obterPosts = function(req, res){
