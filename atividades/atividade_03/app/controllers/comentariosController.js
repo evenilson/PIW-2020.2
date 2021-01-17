5286b8b271a3f91b4fdd359fb2ab5abda3544f41
evenilson <evenilsonlp@gmail.com>
2021-01-17 20:59:10 -0300

    fazendo validações de comentários

--- atividades/atividade_03/app/controllers/comentariosController.js
+++ atividades/atividade_03/app/controllers/comentariosController.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken")
+
 const comentarioModel = require("../models/comentarioModel");
 
 const comentarioView = require("../views/comentarioView");
@@ -25,7 +27,14 @@ module.exports.BuscarComentarioPorId = function(req, res){
 }
 
 module.exports.inserirComentario = function(req, res){
-    let comentario = req.body;
+    let payload = jwt.decode(req.headers.token);
+    let id_usuario_logado = payload.id;
+
+    let comentario = {
+        texto: req.body.texto,
+        id_post: req.body.id_post,
+        id_usuario: id_usuario_logado
+    }
 
     let promise = comentarioModel.create(comentario);
 
@@ -40,11 +49,18 @@ module.exports.inserirComentario = function(req, res){
 
 module.exports.removerComentario = function(req, res){
     let id = req.params.id;
+
+    let payload = jwt.decode(req.headers.token);
+    let id_usuario_logado = payload.id;
     
-    let promise = comentarioModel.findByIdAndRemove(id);
+    let promise = comentarioModel.findOneAndDelete({_id: id, id_usuario: id_usuario_logado});
 
     promise.then(function(comentario){
-        res.status(200).json(comentarioView.render(comentario));
+        if(comentario === null){
+            res.status(404).json({mensagem: "Deu ruim! acesso inválido :("})
+        }else{
+            res.status(200).json(comentarioView.render(comentario));
+        }
     }).catch(function(error){
         res.status(500).json({mensagem:"Deu ruim!", error:error})
     })
--- atividades/atividade_03/app/routes/comentariosRouter.js
+++ atividades/atividade_03/app/routes/comentariosRouter.js
@@ -1,10 +1,10 @@
 const comentariosController = require("../controllers/comentariosController");
+const authController = require("../controllers/authController");
 
 module.exports = function(app){
-    app.get("/comentarios", comentariosController.listarComentarios);
-    app.get("/comentarios/:id", comentariosController.BuscarComentarioPorId);
-
+    app.use("/comentarios", authController.checar);
     app.post("/comentarios", comentariosController.inserirComentario);
-    
+    app.get("/comentarios", comentariosController.listarComentarios);
+    app.get("/comentarios/:id", comentariosController.BuscarComentarioPorId);    
     app.delete("/comentarios/:id", comentariosController.removerComentario);
 }
