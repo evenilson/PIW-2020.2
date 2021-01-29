787358ad015708d01340f7228ef3908d0607bb6a
evenilson <evenilsonlp@gmail.com>
2021-01-29 17:54:55 -0300

    aulas sobre lista de elementos

--- aulas/frontend/sistema_matricula/src/components/pages/PaginaPrincipal/PaginaPrincipal.js
+++ aulas/frontend/sistema_matricula/src/components/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -1,11 +1,30 @@
 import { Navegador } from "../../commom/navegador/Navegador";
 
 function Conteudo() {
-    let nome = "João";
-    let link = (<a href="http://globo.com">Link para a globo</a>)
+    let disciplinas = [
+        {
+            nome: "LMS",
+            codigo: "QXD256"
+        },
+        {
+            nome: "PIW",
+            codigo: "QXD562"
+        },
+        {
+            nome: "SOC",
+            codigo: "QXD784"
+        }
+    ];
+
+    const lis = disciplinas.map((disciplina) =>
+        <li key={disciplina.codigo}>      
+            {disciplina.nome} - {disciplina.codigo}
+        </li>
+    );
+
     return (
-        <div>
-            <p>Meu conteúdo 1+2 {1+2} nome {nome} {link}</p>
+        <div >
+            {lis}
         </div>
     )
 }
