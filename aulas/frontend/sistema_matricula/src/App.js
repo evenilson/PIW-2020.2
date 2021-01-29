9bbec62e09cde1814ffd3e7a914425dcd3637dde
evenilson <evenilsonlp@gmail.com>
2021-01-29 15:33:19 -0300

    aula sore estrutura do projeto

--- aulas/frontend/sistema_matricula/src/App.js
+++ aulas/frontend/sistema_matricula/src/App.js
@@ -1,29 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function LinkParaGoogle() {
-  return <a href="http://google.com">Link para google</a>
-}
+import { PaginaPrincipal } from './components/pages/PaginaPrincipal.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <LinkParaGoogle></LinkParaGoogle>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <PaginaPrincipal></PaginaPrincipal>
+  )
 }
 
 export default App;
---
+++ aulas/frontend/sistema_matricula/src/components/pages/PaginaPrincipal.js
@@ -0,0 +1,15 @@
+
+export function PaginaPrincipal() {
+    return (
+        <div>
+            <nav>
+                <a>Link1</a>
+                <a>Link2</a>
+                <a>Link3</a>
+            </nav>
+            <div>
+                <p>Meu conteúdo</p>
+            </div>
+        </div>
+    )
+}
