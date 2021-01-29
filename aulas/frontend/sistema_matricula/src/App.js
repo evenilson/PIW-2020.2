53772905b8680cb343095d2cc442a235277bcf6d
evenilson <evenilsonlp@gmail.com>
2021-01-29 15:17:10 -0300

    aula sobre componentes React

--- aulas/frontend/sistema_matricula/src/App.js
+++ aulas/frontend/sistema_matricula/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function LinkParaGoogle() {
+  return <a href="http://google.com">Link para google</a>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <LinkParaGoogle></LinkParaGoogle>
         </p>
         <a
           className="App-link"
