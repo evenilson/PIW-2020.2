103a7ffa8c2e150c08e8467ddff4c69b91a0e0da
evenilson <evenilsonlp@gmail.com>
2021-01-30 21:04:51 -0300

    aula sobre classes dinâmicas

--- aulas/frontend/sistema_matricula/src/components/pages/PaginaPrincipal/ConteúdoPaginaPrincipal.js
+++ aulas/frontend/sistema_matricula/src/components/pages/PaginaPrincipal/ConteúdoPaginaPrincipal.js
@@ -1,6 +1,12 @@
-function Card({nome, codigo}) {
+function Card({nome, codigo, disponivel}) {
+    let nomeClasse = "card";
+
+    if(disponivel === false) {
+        nomeClasse += " fundo-vermelho";
+    }
+
     return (
-        <div className="card">
+        <div className={nomeClasse}>
             <h3 className="titulo-card">{nome}</h3>
             <span className="codigo-card">{codigo}</span>
         </div>
@@ -11,24 +17,28 @@ export function Conteudo() {
     let disciplinas = [
         {
             nome: "LMS",
-            codigo: "QXD256"
+            codigo: "QXD256",
+            disponivel: true
         },
         {
             nome: "PIW",
-            codigo: "QXD562"
+            codigo: "QXD562",
+            disponivel: true
         },
         {
             nome: "IHC",
-            codigo: "QXD784"
+            codigo: "QXD784",
+            disponivel: false
         },
         {
             nome: "SOC",
-            codigo: "QXD754"
+            codigo: "QXD754",
+            disponivel: true
         }
     ];
 
     const cards = disciplinas.map((disciplina) =>
-        <Card nome={disciplina.nome} codigo={disciplina.codigo}></Card>
+        <Card nome={disciplina.nome} codigo={disciplina.codigo} disponivel={disciplina.disponivel}></Card>
     );
 
     return (
