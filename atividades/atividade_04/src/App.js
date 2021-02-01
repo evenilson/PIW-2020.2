491d4496c41814f13061c2bb98d64b9d4c2e4342
evenilson <evenilsonlp@gmail.com>
2021-02-01 01:52:48 -0300

    montando o feed completo

--- atividades/atividade_04/src/App.js
+++ atividades/atividade_04/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Feed } from './components/commom/Navigator/Feed/Feed';
 
 import { Navigator } from './components/commom/Navigator/Navigator'
+
 
 function App() {
   return (
     <div className="App">
       <Navigator></Navigator>
+      <Feed></Feed>
     </div>
   );
 }
---
+++ atividades/atividade_04/src/components/commom/Navigator/Post/Post.js
@@ -0,0 +1,16 @@
+import './Post.css'
+
+export function Post (props) {
+    console.log(props)
+    return (
+        
+        <div className="post">
+            <div className="username">{props.username}</div>
+            <div className="text">"{props.texto}"</div>
+            <div className="likes-container">
+                <div className="likes-info">{props.qtdLikes} likes</div>
+                <div className="like-btn"><a href="!#">curtir</a></div>
+            </div>
+        </div>
+    )
+}
