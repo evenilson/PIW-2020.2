a46d072d04f57755523537588c14a4bf99c9230b
evenilson <evenilsonlp@gmail.com>
2021-02-16 20:01:54 -0300

    aula sobre redirecionamento para as rotas

--- aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
+++ aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
@@ -1,14 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Navegador.css'
 
 export function Navegador () {
 
-    let Link = ({linkTexto}) => (<a className="link-navegador" href="http://globo.com">{linkTexto}</a>);
-
     return (
         <nav className="navegador">
-            <Link linkTexto="link1"></Link>
-            <Link linkTexto="link3"></Link>
-            <Link linkTexto="link2"></Link>
+            <Link className="link-navegador" to="/">Página Principal</Link>
+            <Link className="link-navegador" to="/matriculas">Listar Matriculas</Link>
         </nav>
     )
 }
--- aulas/frontend/sistema_matricula/src/components/pages/PaginaListarMatriculas/PaginaListarMatriculas.js
+++ aulas/frontend/sistema_matricula/src/components/pages/PaginaListarMatriculas/PaginaListarMatriculas.js
@@ -1,6 +1,9 @@
+import { Navegador } from "../../commom/navegador/Navegador";
+
 export function PaginaListarMatriculas() {
     return (
         <div>
+            <Navegador></Navegador>
             <h1>Página de Matricula</h1>
         </div>
     )
