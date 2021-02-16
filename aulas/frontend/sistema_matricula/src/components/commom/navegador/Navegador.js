edef31621c98ea174fa9ee5754b795e44ac7129e
evenilson <evenilsonlp@gmail.com>
2021-02-16 20:11:05 -0300

    aula sobre estilização de links ativos

--- aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
+++ aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navegador.css'
 
 export function Navegador () {
-
     return (
         <nav className="navegador">
-            <Link className="link-navegador" to="/">Página Principal</Link>
-            <Link className="link-navegador" to="/matriculas">Listar Matriculas</Link>
+            <NavLink exact className="link-navegador" to="/">Página Principal</NavLink>
+            <NavLink className="link-navegador" to="/matriculas">Listar Matriculas</NavLink>
         </nav>
     )
 }
