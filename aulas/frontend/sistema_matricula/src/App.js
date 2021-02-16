52e29b29a4474c70e44c38380bf9830a1c9730bf
evenilson <evenilsonlp@gmail.com>
2021-02-16 19:45:41 -0300

    aula sobre React Router DOM

--- aulas/frontend/sistema_matricula/src/App.js
+++ aulas/frontend/sistema_matricula/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 
 import { PaginaPrincipal } from './components/pages/PaginaPrincipal/PaginaPrincipal'
+import { PaginaListarMatriculas } from './components/pages/PaginaListarMatriculas/PaginaListarMatriculas'
 
 function App() {
   return (
-    <PaginaPrincipal></PaginaPrincipal>
+    <BrowserRouter>
+      <Route exact path="/">
+        <PaginaPrincipal></PaginaPrincipal>
+      </Route>
+      <Route path="/matriculas">
+        <PaginaListarMatriculas></PaginaListarMatriculas>
+      </Route>
+    </BrowserRouter>
+    
   )
 }
 
---
+++ aulas/frontend/sistema_matricula/src/components/pages/PaginaListarMatriculas/PaginaListarMatriculas.js
@@ -0,0 +1,7 @@
+export function PaginaListarMatriculas() {
+    return (
+        <div>
+            <h1>Página de Matricula</h1>
+        </div>
+    )
+}
