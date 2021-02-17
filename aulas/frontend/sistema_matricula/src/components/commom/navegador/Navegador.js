a07a761b198764d127a51c82d2883fa577094de1
evenilson <evenilsonlp@gmail.com>
2021-02-16 21:07:29 -0300

    aula de redirecionamento para rotas com parâmetro

--- aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
+++ aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
@@ -2,10 +2,12 @@ import { NavLink } from 'react-router-dom';
 import './Navegador.css'
 
 export function Navegador () {
+    let id = 15;
     return (
         <nav className="navegador">
             <NavLink exact className="link-navegador" to="/">Página Principal</NavLink>
             <NavLink className="link-navegador" to="/matriculas">Listar Matriculas</NavLink>
+            <NavLink className="link-navegador" to={"/matriculas/"+id}>Detalhes de matricula</NavLink>
         </nav>
     )
 }
