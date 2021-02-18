0164b3f35e88cf6bb13237310b43249dab200001
evenilson <evenilsonlp@gmail.com>
2021-02-17 23:51:09 -0300

    criando os components para post e criando as rotas

--- atividades/atividade_05/src/App.js
+++ atividades/atividade_05/src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route} from 'react-router-dom'
+
+
 import './App.css';
 import { HomePage } from './components/Pages/HomePage/HomePage';
+import { PostPage } from './components/Pages/PostPage/PostPage';
 
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <BrowserRouter>
+        <Route exact path="/">
+          <HomePage></HomePage>
+        </Route>
+        <Route path="/post">
+          <PostPage></PostPage>
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ atividades/atividade_05/src/components/Pages/PostPage/PostPage.js
@@ -0,0 +1,9 @@
+import { Navigator } from "../../commom/Navigator/Navigator"
+
+export function PostPage(){
+    return(
+        <>
+            <Navigator></Navigator>
+        </>
+    )
+}
