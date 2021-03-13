0129de30f2319a4638c292ed19286105b6d2fca7
evenilson <evenilsonlp@gmail.com>
2021-03-13 20:12:41 -0300

    Ajustando o navegador

--- aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
+++ aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
@@ -5,9 +5,15 @@ export function Navegador () {
     let id = 15;
     return (
         <nav className="navegador">
-            <NavLink exact className="link-navegador" to="/">Página Principal</NavLink>
-            <NavLink className="link-navegador" to="/matriculas">Listar Matriculas</NavLink>
-            <NavLink className="link-navegador" to={"/matriculas/"+id}>Detalhes de matricula</NavLink>
+            <div>
+                <NavLink exact className="link-navegador" to="/">Página Principal</NavLink>
+                <NavLink className="link-navegador" to="/matriculas">Listar Matriculas</NavLink>
+                <NavLink className="link-navegador" to={"/matriculas/"+id}>Detalhes de matricula</NavLink>
+            </div>
+            <div>
+                <NavLink className="link-navegador" to={"/cadastrar/"}>Cadastrar</NavLink>
+                <NavLink className="link-navegador" to={"/login/"}>Login</NavLink>
+            </div>
         </nav>
     )
 }
