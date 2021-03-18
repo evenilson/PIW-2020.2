58bdb06f532dd68c0f77a8acc06cf33773206433
evenilson <evenilsonlp@gmail.com>
2021-03-17 21:06:17 -0300

    Aula de revisitando navegador e implementando logout

--- aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
+++ aulas/frontend/sistema_matricula/src/components/commom/navegador/Navegador.js
@@ -1,19 +1,44 @@
 import { NavLink } from 'react-router-dom';
 import './Navegador.css'
 
-export function Navegador () {
-    let id = 15;
+import {AuthContext} from '../../../App'
+import { useContext } from 'react';
+
+function NavegadorLogado({nome}){
+    const {setAuth} = useContext(AuthContext);
     return (
         <nav className="navegador">
             <div>
                 <NavLink exact className="link-navegador" to="/">Página Principal</NavLink>
                 <NavLink className="link-navegador" to="/matriculas">Listar Matriculas</NavLink>
-                <NavLink className="link-navegador" to={"/matriculas/"+id}>Detalhes de matricula</NavLink>
             </div>
+            <div>
+                <strong>{nome}</strong>
+                <NavLink className="link-navegador" to="/" onClick={()=>{setAuth({token: null, name: null})}}>Logout</NavLink>
+
+            </div>
+        </nav>
+    )
+}
+
+function NavegadorNaoLogado(){
+    return(
+        <nav className="navegador">
+            <strong>Seja bem vindo</strong>
             <div>
                 <NavLink className="link-navegador" to={"/cadastrar/"}>Cadastrar</NavLink>
                 <NavLink className="link-navegador" to={"/login/"}>Login</NavLink>
             </div>
         </nav>
+    )
+    
+}
+
+export function Navegador () {
+   const {auth} = useContext(AuthContext);
+    return (
+        <>
+            {auth.token === null ? <NavegadorNaoLogado /> : <NavegadorLogado nome={auth.nome}/>}
+        </>
     )
 }
