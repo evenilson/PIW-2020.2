b139cb79db543a706d5c35eaa8232bc27fdc1833
evenilson <evenilsonlp@gmail.com>
2021-03-25 15:53:44 -0300

    Criando o contexto da autenticação usando localStorage para persistencia

--- atividades/atividade_06/src/App.js
+++ atividades/atividade_06/src/App.js
@@ -1,4 +1,5 @@
-import {Route, Router} from 'react-router-dom'
+import { Route, Router } from 'react-router-dom';
+import { createContext, useState } from 'react';
 
 import history from './history'
 
@@ -8,24 +9,34 @@ import { PostPage } from './components/Pages/PostPage/PostPage';
 import { SignUpPage } from './components/Pages/SignUpPage/SignUpPage';
 import { SignInPage } from './components/Pages/SignInPage/SignInPage';
 
-
+export const authContext = createContext(null);
 
 function App() {
+  const [auth, setAuth] = useState({token: localStorage.getItem("token"), nome: localStorage.getItem("nome")})
+
+  function setAuthLocalStorage(newAuth) {
+    setAuth(newAuth)
+    localStorage.setItem("token", newAuth.token)
+    localStorage.setItem("nome", newAuth.nome)
+  }
+
   return (
-    <Router history={history}>
-      <Route exact path="/">
-        <HomePage></HomePage>
-      </Route>
-      <Route path="/post">
-        <PostPage></PostPage>
-      </Route>
-      <Route path="/cadastrar">
-        <SignUpPage />
-      </Route>
-      <Route path="/login">
-        <SignInPage />
-      </Route>
-    </Router>
+    <authContext.Provider value={{auth: auth, setAuth: setAuthLocalStorage}}>
+      <Router history={history}>
+        <Route exact path="/">
+          <HomePage></HomePage>
+        </Route>
+        <Route path="/post">
+          <PostPage></PostPage>
+        </Route>
+        <Route path="/cadastrar">
+          <SignUpPage />
+        </Route>
+        <Route path="/login">
+          <SignInPage />
+        </Route>
+      </Router>
+    </authContext.Provider>
   );
 }
 
