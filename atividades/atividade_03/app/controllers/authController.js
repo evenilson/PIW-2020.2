654f18e54d7a6109263c5a1221fe0919551c64f9
evenilson <evenilsonlp@gmail.com>
2021-03-25 16:27:00 -0300

    Ajustando banco para retorna o nome do usuario quando logar

--- atividades/atividade_03/app/controllers/authController.js
+++ atividades/atividade_03/app/controllers/authController.js
@@ -8,7 +8,7 @@ module.exports.logar = function(req, res){
         .then(function(usuario){
             if(bcrypt.compareSync(req.body.senha, usuario.senha)){
                 let token = jwt.sign({id:usuario._id}, "senha_secreta")
-                res.status(200).json({token:token});
+                res.status(200).json({token:token, nome:usuario.nome});
             }
             else{
                 res.status(401).send("credenciais erradas")
