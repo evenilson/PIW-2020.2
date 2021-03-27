4158e831c82db77fa5e9990a79289557b5dd035d
evenilson <evenilsonlp@gmail.com>
2021-03-27 00:08:17 -0300

    Fizalizando função de adicionar comentário no post

--- atividades/atividade_06/src/components/Pages/HomePage/Post/Post.js
+++ atividades/atividade_06/src/components/Pages/HomePage/Post/Post.js
@@ -1,6 +1,10 @@
 import { useContext, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { AiOutlineLike } from 'react-icons/ai';
+
 import { getPostComents } from '../../../../api/postAPI';
+import { setComentAxios } from '../../../../api/comentsAPI'
+
 import { authContext } from '../../../../App';
 
 import './Post.css'
@@ -22,19 +26,33 @@ function Coment(props) {
 
 export function Post(props) {
     const [coments, setComents] = useState([])
+    const { register, handleSubmit } = useForm()
+
 
     const { auth } = useContext(authContext)
 
-    useEffect(()=>{
+
+    function getComentsFunction() {
         getPostComents(auth.token, props.id).then((response) => {
             setComents(response.data)
         }).catch((error) => {
             console.log(error);
         })
-    },[])
+    }
 
+    useEffect(() => {
+        getComentsFunction();
+    }, [])
 
-    const makeComents = coments.map((coment) => 
+    function setComentFunction(coment) {
+        setComentAxios(auth.token, coment, props.id).then((response) => {
+            getComentsFunction();
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    const makeComents = coments.map((coment) =>
         <Coment key={coment.id} text={coment.texto} name={coment.nome_usuario} />
     );
 
@@ -54,11 +72,11 @@ export function Post(props) {
             <div className="comments-container">
                 {makeComents}
 
-                <form className="write-comment">
+                <form className="write-comment" onSubmit={handleSubmit(setComentFunction)}>
                     <div className="user-img img-comment">
                         <img src="https://i.pinimg.com/originals/8b/d3/1d/8bd31d79258cb5e51cd8884986ced870.jpg" alt="Foto do usuario logado" />
                     </div>
-                    <input className="input" placeholder="Escreva um comentário" />
+                    <input type="text" className="input" name="texto" ref={register} placeholder="Escreva um comentário" />
                 </form>
             </div>
         </div>
